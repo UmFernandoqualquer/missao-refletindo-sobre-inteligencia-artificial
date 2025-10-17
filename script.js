63cdde8218d3aa2f061d4cef299dc7a5adcfa988
UmFernandoqualquer <fernando.cedorak@escola.pr.gov.br>
2025-10-17 07:49:41 -0300

    ia

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternantivas = document.querySelector(".caixa-alternantivas");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
